0bdaaf84ad302132bf1dfd936cc115a4425f2f81
Suellen Abreu <161541448+SuellenAbreu@users.noreply.github.com>
2025-09-15 00:09:22 -0300

    Create desafios_projeto.java

---
+++ desafios_projeto.java
@@ -0,0 +1,5 @@
+// A maior dificuldade em realizar esse desafio foi em subir o projeto no Github, primeiro foi por causa do tamanho do arquivo 
+//e depois por causa de alguns erros ao fazer o envio.
+//No final deu tudo certo
+//Enviei o link do diagrama, foi a unica maneira que consegui fazer o envio. 
+// Apos muitas brigas com o git desktop e o github, enfim consegui subir a imagem do diagrama. UUUFA!

11422412f664b560efbf222eb506dd7324d9223d
Suellen Abreu <161541448+SuellenAbreu@users.noreply.github.com>
2025-09-14 23:58:38 -0300

    Create Link_Desafio1_DIAGRAMA_1.r

---
+++ Link_Desafio1_DIAGRAMA_1.r
@@ -0,0 +1,6 @@
+Desafio1_Code_Girl
+Santander Code Girls - 2025
+
+O arquivo completo está disponível [aqui no Google Drive](https://drive.google.com/drive/folders/16fr69yfLWMGPXjN_47DPcKWVU3R6V3rw?usp=drive_link).
+
+[DESAFIO_1](https://drive.google.com/drive/folders/16fr69yfLWMGPXjN_47DPcKWVU3R6V3rw?usp=drive_link)
